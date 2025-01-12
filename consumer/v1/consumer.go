e1ea0267e145cd8b7ea1eddcef3f9220ed4d75fa
MohitKrishna <arumallamohit.krishna@infra.market>
2025-01-12 18:09:15 +0530

    made consumer go rouitine

--- consumer/v1/consumer.go
+++ consumer/v1/consumer.go
@@ -16,22 +16,25 @@ type ConsumerHandler struct {
 
 func (ch *ConsumerHandler) StartConsuming() {
 	// Subscribe to the topic
-	err := ch.Consumer.Subscribe(ch.Topic, nil)
-	if err != nil {
-		log.Fatalf("Failed to subscribe to topic %s: %v", ch.Topic, err)
-	}
-
 	fmt.Println("Consumer started, waiting for messages...")
-
-	// Consume messages in a loop
-	for {
-		msg, err := ch.Consumer.ReadMessage(-1)
+	go func() {
+		// Subscribe to the topic
+		err := ch.Consumer.Subscribe(ch.Topic, nil)
 		if err != nil {
-			log.Printf("Error reading message: %v", err)
-			continue
+			log.Fatalf("Failed to subscribe to topic %s: %v", ch.Topic, err)
 		}
 
-		// Print the consumed message
-		fmt.Printf("Message received: %s\n", string(msg.Value))
-	}
+		// Consume messages in a loop
+		for {
+			msg, err := ch.Consumer.ReadMessage(-1)
+			if err != nil {
+				log.Printf("Error reading message: %v", err)
+				continue
+			}
+
+			// Print the consumed message
+			fmt.Printf("Message received: %s\n", string(msg.Value))
+		}
+	}()
+
 }
